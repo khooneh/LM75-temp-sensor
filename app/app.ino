47b7957e4bb01646eccab7e4bdf43aae27feb2f1
Nomaan Ahgharian <nahgharian@frontlineed.com>
2021-11-07 22:00:02 -0600

    Add a webserver

--- app/app.ino
+++ app/app.ino
@@ -1,6 +1,15 @@
 
 #define VERSION "1.0"
 
+//---- Web Server ----//
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h> 
+
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>   // Include the WebServer library
+//
+
 #include <inttypes.h>
 #include <Wire.h>
 
@@ -16,12 +25,48 @@ TempI2C_LM75 termo = TempI2C_LM75(0x48,TempI2C_LM75::ten_bits);
 
 TM1637Display display(CLK, DIO);
 
+// WiFi
+ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+
+void handleRoot();              // function prototypes for HTTP handlers
+void handleNotFound();
+
+String SSID = " "; 
+String PW = " "; 
+
 void setup()
 {
     Serial.begin(9600);
     display.setBrightness(0x0a);
+
+    Serial.println("Temp ...\n");
     Serial.print(termo.getTemp());
-  
+    wifiMulti.addAP(SSID.c_str(), PW.c_str());
+
+    Serial.println("Connecting ...");
+    int i = 0;
+    while (wifiMulti.run() != WL_CONNECTED) { 
+      delay(250);
+      Serial.print(".");
+    }
+    Serial.print("Connected to ");
+    Serial.println(WiFi.SSID());               
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP()); 
+
+    if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
+      Serial.println("mDNS responder started");
+    } else {
+      Serial.println("Error setting up MDNS responder!");
+    }
+
+    server.on("/", handleRoot);                
+    server.onNotFound(handleNotFound); 
+
+    server.begin();                           
+    Serial.println("HTTP server started");
+
 }
 
 void loop()
@@ -29,8 +74,8 @@ void loop()
     double tempC = termo.getTemp();
     int tempF = (tempC * 9 / 5) + 32;
     
-    Serial.print(tempF);
-    Serial.println(" oF");
+//    Serial.print(tempF);
+//    Serial.println(" oF");
    
     const uint8_t fahrenheit[] = {
       SEG_A | SEG_B | SEG_F | SEG_G,  // Circle
@@ -39,4 +84,15 @@ void loop()
 
     display.showNumberDec(tempF, false, 2, 0);
     display.setSegments(fahrenheit, 2, 2);
+
+    // WiFi 
+    server.handleClient();  
+}
+
+void handleRoot() {
+  server.send(200, "text/plain", "Hello world!"); 
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found");  
 }
