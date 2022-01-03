5afb601d6a25b8c474806f8bfaf7451da997503e
Nomaan Ahgharian <nahgharian@frontlineed.com>
2022-01-03 17:21:58 -0600

    Add C/F API and display toggle

--- app/app.ino
+++ app/app.ino
@@ -26,73 +26,132 @@ TempI2C_LM75 termo = TempI2C_LM75(0x48,TempI2C_LM75::ten_bits);
 TM1637Display display(CLK, DIO);
 
 // WiFi
-ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
-ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+ESP8266WiFiMulti wifiMulti;      
+ESP8266WebServer server(80);     
 
-void handleRoot();              // function prototypes for HTTP handlers
+
+#include <WiFiManager.h>         
+
+WiFiManager wifiManager;
+
+bool isCelsius = false;
+
+
+// display change button 
+#define DIS_PIN D8
+int switchStatusLast = LOW;
+
+void handleRoot();              
 void handleNotFound();
 
-String SSID = " "; 
-String PW = " "; 
 
-void setup()
-{
-    Serial.begin(9600);
-    display.setBrightness(0x0a);
+void showF() {
+    int temp = getTempF(); 
 
-    Serial.println("Temp ...\n");
-    Serial.print(termo.getTemp());
-    wifiMulti.addAP(SSID.c_str(), PW.c_str());
-
-    Serial.println("Connecting ...");
-    int i = 0;
-    while (wifiMulti.run() != WL_CONNECTED) { 
-      delay(250);
-      Serial.print(".");
-    }
-    Serial.print("Connected to ");
-    Serial.println(WiFi.SSID());               
-    Serial.print("IP address:\t");
-    Serial.println(WiFi.localIP()); 
-
-    if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
-      Serial.println("mDNS responder started");
-    } else {
-      Serial.println("Error setting up MDNS responder!");
-    }
-
-    server.on("/", handleRoot);                
-    server.onNotFound(handleNotFound); 
-
-    server.begin();                           
-    Serial.println("HTTP server started");
-
-}
-
-void loop()
-{   
-    double tempC = termo.getTemp();
-    int tempF = (tempC * 9 / 5) + 32;
-    
-//    Serial.print(tempF);
-//    Serial.println(" oF");
-   
     const uint8_t fahrenheit[] = {
       SEG_A | SEG_B | SEG_F | SEG_G,  // Circle
       SEG_A | SEG_F | SEG_E | SEG_G   // F
     };
 
-    display.showNumberDec(tempF, false, 2, 0);
+    display.showNumberDec(temp, false, 2, 0);
     display.setSegments(fahrenheit, 2, 2);
 
-    // WiFi 
-    server.handleClient();  
+    isCelsius = false; 
+
+    server.send(200, "text/plain", "Display should show Fahrenheit"); 
+
+}
+
+void showC() {
+    int temp = termo.getTemp();
+    const uint8_t celsius[] = {
+      SEG_A | SEG_B | SEG_F | SEG_G,  // Circle
+      SEG_A | SEG_F | SEG_E | SEG_D   // C
+    };
+
+    display.showNumberDec(temp, false, 2, 0);
+    display.setSegments(celsius, 2, 2);
+
+    isCelsius = true; 
+
+    server.send(200, "text/plain", "Display should show Celsius"); 
+}
+
+int getTempF() {
+    double tempC = termo.getTemp();
+    int tempF = (tempC * 9 / 5) + 32;
+
+    return tempF;
+}
+
+void sendF() {
+  server.send(200, "text/plain", String(getTempF()));
+}
+
+void sendC() {
+  server.send(200, "text/plain", String(termo.getTemp()));
 }
 
 void handleRoot() {
-  server.send(200, "text/plain", "Hello world!"); 
+  server.send(200, "text/plain", String(getTempF())); 
 }
 
 void handleNotFound(){
   server.send(404, "text/plain", "404: Not found");  
+}
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(DIS_PIN, INPUT);
+
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    Serial.begin(9600);
+    display.setBrightness(0x0a);
+
+    Serial.print("Temp ...\n");
+    Serial.print(termo.getTemp());
+    wifiManager.autoConnect("LM75-wifi-sensor");
+
+    server.on("/", handleRoot);                
+    // These will report the temp 
+    server.on("/api/temp/f", sendF);                
+    server.on("/api/temp/c", sendC);   
+
+
+    // these changes the LED display
+    server.on("/temp/f", showF);                
+    server.on("/temp/c", showC);  
+
+    server.onNotFound(handleNotFound); 
+
+    server.begin();                           
+    Serial.println("HTTP server started");
+
+    if (WiFi.status() == WL_CONNECTED) {
+      digitalWrite(LED_BUILTIN, LOW);
+    } 
+ 
+}
+
+
+void loop() {   
+    if (isCelsius) {
+      showC();
+    } else {
+      showF();
+    }
+
+    int switchStatus = digitalRead(DIS_PIN);   
+
+    if (switchStatusLast != switchStatus) {
+      switchStatusLast = switchStatus;
+      if (switchStatus == HIGH) {
+        isCelsius = !isCelsius;
+      }
+    }
+
+    // WiFi 
+    server.handleClient();  
 }
